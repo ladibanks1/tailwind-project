5c1c0cd4504224de79107f466eb25cdf55faa244
Oladipupo <oladipupobankole24@gmail.com>
2024-07-11 00:10:17 +0100

    First tailwind project

---
+++ MODULES/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../HTML/*.html",
+    "../CSS/*.css",
+  ],
+  theme: {
+    extend: {
+     colors:{
+      "gold" : {
+        "100": "#ffd700"
+      },
+      "navy-blue" : "#00008B",
+      "forest-green": "#228B22",
+      "wine" : "rgb(162, 28, 49)"
+     },
+     spacing:{
+        "50" : "50%",
+        "75" : "75%"
+     }
+    },
+  },
+  plugins: [],
+}
+
